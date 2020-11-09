d7d1b7d64f81001557527c88495f9414237505da
Piyush Anand <piyushanand.1221@gmail.com>
2020-11-09 20:35:10 +0530

    Functions vs class based components

---
+++ classbased_vs_functionbased/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+// * Class based components
+class App extends React.Component {
+  render() {
+    return (
+      // <div>I am a class Based Component</div>
+      <div className="App">
+        <header className="App-header">I am a class Based Component</header>
+      </div>
+    );
+  }
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">hi</header>
+//     </div>
+//   );
+// }
+
+export default App;
