6a682f5aadc1cd83463bf8f790f8b7ccda35e52e
Piyush Anand <piyushanand.1221@gmail.com>
2020-11-24 16:09:15 +0530

    title added

--- github_explorer/src/App.js
+++ github_explorer/src/App.js
@@ -1,13 +1,12 @@
+import React from "react";
 import "./App.css";
+
+import Search from "./components/Search";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World !!!</h1>
-
-        <button className="btn btn-success">Bootstrap Button</button>
-      </header>
+    <div>
+      <Search />
     </div>
   );
 }
---
+++ github_explorer/src/components/Search.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Search extends React.Component {
+  render() {
+    return (
+      <div>
+        <input type="text" name="username" placeholder="Enter Username" />
+      </div>
+    );
+  }
+}
