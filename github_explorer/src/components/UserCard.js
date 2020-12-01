c8254939accfa6a90cc88dc3e5fd9725ab674bbb
Piyush Anand <piyushanand.1221@gmail.com>
2020-12-01 23:31:23 +0530

    added call button and media queries

--- github_explorer/src/components/UserCard.js
+++ github_explorer/src/components/UserCard.js
@@ -2,11 +2,17 @@ import React from "react";
 
 export default function UserCard({ user }) {
   return (
-    <div className="card col-4">
-      <div className="card-body">
-        <img src={user.avatar_url} alt="Avatar" />
-        <h3 className="card-title">{user.name}</h3>
-        <p>{user.bio}</p>
+    <div>
+      <div className="card">
+        <div className="card-body">
+          <img
+            className="rounded mx-auto d-block"
+            src={user.avatar_url}
+            alt="Avatar"
+          />
+          <h3 className="card-title">{user.name}</h3>
+          <p>{user.bio}</p>
+        </div>
       </div>
     </div>
   );
