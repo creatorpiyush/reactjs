26166953ef6a9283733b32829b39baad72b22870
Piyush Anand <piyushanand.1221@gmail.com>
2020-12-02 19:02:32 +0530

    User Card for user Information

--- github_explorer/src/components/UserCard.js
+++ github_explorer/src/components/UserCard.js
@@ -4,17 +4,17 @@ import { Container, Row, Col, Card, Button } from "react-bootstrap";
 export default function UserCard({ user }) {
   return (
     <div>
-      <Container fluid="sm">
+      <Container fluid="sm" className="pt-5">
         <Row>
           <Col>
-            <Card style={{ width: "18rem" }}>
+            <Card border="danger" style={{ width: "18rem" }}>
               <Card.Header>User Profile</Card.Header>
               <Card.Img variant="top" src={user.avatar_url} />
               <Card.Body>
                 <Card.Title>{user.name}</Card.Title>
                 <Card.Text>{user.bio}</Card.Text>
                 <a href={user.html_url} rel="noreferrer" target="_blank">
-                  <Button variant="primary">Go To User Profile</Button>
+                  <Button variant="success">Go To User Profile</Button>
                 </a>
               </Card.Body>
             </Card>
