af212e72db05c12f3b11e1ee2cbd2a9f09ae7106
Piyush Anand <piyushanand.1221@gmail.com>
2020-12-02 14:53:42 +0530

    User Card for user Information

--- github_explorer/src/components/UserCard.js
+++ github_explorer/src/components/UserCard.js
@@ -1,19 +1,26 @@
 import React from "react";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
 
 export default function UserCard({ user }) {
   return (
     <div>
-      <div className="card">
-        <div className="card-body">
-          <img
-            className="rounded mx-auto d-block"
-            src={user.avatar_url}
-            alt="Avatar"
-          />
-          <h3 className="card-title">{user.name}</h3>
-          <p>{user.bio}</p>
-        </div>
-      </div>
+      <Container fluid="sm">
+        <Row>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Header>User Profile</Card.Header>
+              <Card.Img variant="top" src={user.avatar_url} />
+              <Card.Body>
+                <Card.Title>{user.name}</Card.Title>
+                <Card.Text>{user.bio}</Card.Text>
+                <a href={user.html_url} rel="noreferrer" target="_blank">
+                  <Button variant="primary">Go To User Profile</Button>
+                </a>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
--- github_explorer/src/index.js
+++ github_explorer/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "react-bootstrap";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
